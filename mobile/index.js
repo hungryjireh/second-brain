5d5d7c6fba1384f8ed380a84311217b7f4e73425
hungryjireh <hungryjireh@gmail.com>
2026-05-16 21:20:16 +0800

    refactor: move index.js styles to global.css file

--- mobile/index.js
+++ mobile/index.js
@@ -1,15 +1,8 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import { theme } from './src/theme';
-
 if (typeof document !== 'undefined') {
-  const baseColor = theme.colors.bgBase;
-  document.documentElement.style.backgroundColor = baseColor;
-  document.body.style.backgroundColor = baseColor;
-  document.body.style.margin = '0';
-  const root = document.getElementById('root');
-  if (root) root.style.backgroundColor = baseColor;
+  require('./src/global.css');
 }
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
